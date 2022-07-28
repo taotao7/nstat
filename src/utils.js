702af38a0b61135c75cd0b42851e612408c6ac3d
taotao7 <moca_tao7@foxmail.com>
2022-07-28 23:28:02 +0800

    feat: add utils function

--- src/utils.js
+++ src/utils.js
@@ -20,18 +20,51 @@ const checkGit = () => {
   return true;
 };
 
-const getAllAuthor = () => {
+// regx time
+const checkTime = (start, end) => {
+  const regx = /\d{4}(-\d{2}){2}/;
+  if (regx.exec(start) && regx.exec(end)) {
+    return true;
+  }
+  console.log(
+    chalk.red(
+      "The date entered does not conform to the specification e.g. 2022-11-21"
+    )
+  );
+  return false;
+};
+
+const getAuthorCommit = (start, end) => {
+  let stat;
+
+  if (start && end) {
+    if (!checkTime(start, end)) {
+      return;
+    }
+
+    if (checkGit()) {
+      console.log(
+        shell.exec(
+          `git log --pretty=tformat: --numstat --since=${start} --until=${end}`,
+          { silent: true }
+        ).stdout
+      );
+      return;
+    }
+  }
+
   if (checkGit()) {
     console.log(
-      shell.exec(" git log --pretty=tformat: --numstat", { silent: true })
+      shell.exec(`git log --pretty=tformat: --numstat `, { silent: true })
         .stdout
     );
+    return;
   }
 };
 
-getAllAuthor();
+getAuthorCommit();
 
 module.exports = {
   checkGit,
-  getAllAuthor,
+  getAuthorCommit,
 };
